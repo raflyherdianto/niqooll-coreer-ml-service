d378d4a687a68c2d602666b56439a0a489163bdb
Rafly Herdianto <raflyherdianto25@gmail.com>
2025-06-12 06:51:21 +0700

    Update post-build script to include punkt_tab in NLTK data download

--- post_build.sh
+++ post_build.sh
@@ -3,7 +3,7 @@
 # Script ini akan dijalankan setelah proses build Oryx selesai
 echo "Menjalankan post-build script..."
 
-# Mengunduh data NLTK yang diperlukan ke dalam direktori yang akan dipaketkan
-python -m nltk.downloader -d /home/site/wwwroot/nltk_data stopwords punkt
+# Menambahkan punkt_tab ke daftar download
+python -m nltk.downloader -d /home/site/wwwroot/nltk_data stopwords punkt punkt_tab
 
 echo "Proses download data NLTK selesai."
